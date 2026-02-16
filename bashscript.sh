11cbe9c108c64af470f261fcc30091c68dd68d24
Moamen Mostafa <31764526+MoamenMostafaK@users.noreply.github.com>
2026-02-16 12:57:33 +0200

    PUSHAIAAA

--- bashscript.sh
+++ bashscript.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 echo "hello from bashscript"
+
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover,iam a Dragon ... RAWA" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls 
